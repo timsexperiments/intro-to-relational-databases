a354af4181ff0347d1e7e5a0622a801a0854244a
William Altemus <altemuswilliam@gmail.com>
2025-05-09 02:09:22 -0500

    Did Homework Lesson 1

---
+++ submissions/homework_1/cc1759/hm.sql
@@ -0,0 +1,20 @@
+-- create and use database
+CREATE DATABASE IF NOT EXISTS lesson1;
+USE lesson1;
+
+-- 
+CREATE TABLE Emilys_fav_books (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(150),
+    author VARCHAR(100),
+    year_published INT
+);
+
+-- insert rows/books into table
+INSERT INTO Emilys_fav_books (title, author, year_published) 
+    VALUES ('Cursed Princess Club', 'JambCat', 2019);
+INSERT INTO Emilys_fav_books (title, author, year_published) 
+    VALUES ('Grace (but said in a French accent)', 'Mary Casanova', 2015);
+
+-- display all rows/books in the table
+SELECT * FROM Emilys_fav_books;
